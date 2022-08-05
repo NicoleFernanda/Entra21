db7e185cf8f51b743df9ab33106a9a54e66ad0ad
Nicole F. Rodrigues Alves <104005596+NicoleFernanda@users.noreply.github.com>
2022-08-05 11:28:18 -0300

    Add files via upload

---
+++ java avancado/Lista 2/Conta.java
@@ -0,0 +1,31 @@
+package lista2;
+
+public class Conta {
+	
+	int numero;
+	double saldo;
+	String nome;
+	String agencia;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
